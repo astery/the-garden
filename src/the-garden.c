a7f167e1411dc7cdb7dfb4e9188db326c50570d4
Andrey Akulov <andreyshark@gmail.com>
2016-04-02 18:04:19 +0300

    Initial commit.

---
+++ src/the-garden.c
@@ -0,0 +1,36 @@
+/*
+ ============================================================================
+ Name        : the-garden.c
+ Author      : Astery & Z
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+
+int main(int argc, char* argv[]) {
+    // Start SDL2
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    // Create a Window in the middle of the screen
+    SDL_Window *window = 0;
+
+    window = SDL_CreateWindow("Hello World!",
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              640, 480,
+                              SDL_WINDOW_SHOWN);
+
+    // Delay so that we can see the window appear
+    SDL_Delay(2000);
+
+    // Cleanup and Quit
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
